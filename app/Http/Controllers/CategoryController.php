f599e1fbab9ebf7d32d249c339511b37d089a6dd
kritsanaraka <78993140+kritsanaraka@users.noreply.github.com>
2021-02-14 11:08:55 +0700

    อัปเดดก่อนuser

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -12,7 +12,7 @@ class CategoryController extends Controller
         $data = [
             'categories' => Category::all(),
         ];
-        return view('category.index',$data);
+        return view('category.index', $data);
     }
     public function create()
     {
@@ -26,6 +26,29 @@ class CategoryController extends Controller
         $category->name = $name;
         $category->save();
 
+        return redirect('/category');
+    }
+    public function edit($id)
+    {
+        $category = Category::find($id);
+        $data = [
+            'category' => $category
+        ];
+        return view('category.edit', $data);
+    }
+    public function update(Request $request, $id)
+    {
+        $name=$request->input('name');
+        $category = Category::find($id);
+        $category->name=$name;
+        $category->save();
+
+        return redirect('/category');
+    }
+    public function delete($id)
+    {
+        $category = Category::find($id);
+        $category->delete();
         return redirect('/category');
     }
 }
